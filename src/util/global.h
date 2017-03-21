8fa2ecca9a90dd44247a08080ed4dc814abe4e0e
Daniel Cole <roxas.dan.sora@hotmail.co.uk>
2017-03-21 12:31:46 +0000

    Restore better default values for chunk loading

--- src/util/global.h
+++ src/util/global.h
@@ -6,11 +6,11 @@
 namespace Game
 {
 	const int blocktypes = 9;
-	const int CHUNKS_RANGE = 5;
+	const int CHUNKS_RANGE = 10;
 	const int CHUNKS_X = 2*CHUNKS_RANGE + 1;
 	const int CHUNKS_Y = 1;
 	const int CHUNKS_Z = 2*CHUNKS_RANGE + 1;
-	const int FRAME_RANGE = 4;
+	const int FRAME_RANGE = 5;
 	//temp
 	const int DIFF = 2 * CHUNKS_RANGE;
 	const int MAX_TASKS = (CHUNKS_RANGE + 1) * (CHUNKS_RANGE + 10);
